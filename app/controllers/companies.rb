b7afd7a2810071e3fb3d638d5bc7a6079697edf3
havestopchai <havestopchai@yahoo.com.tw>
2021-06-06 17:30:05 +0800

    [view]

--- app/controllers/companies.rb
+++ app/controllers/companies.rb
@@ -8,11 +8,11 @@ module ISSInternship
         # GET /companies/
         routing.get do
           if @current_account.logged_in?
-            project_list = GetAllCompanies.new(App.config).call(@current_account)
+            company_list = GetAllCompanies.new(App.config).call(@current_account)
 
             companies = Companies.new(company_list)
 
-            view :projects_all,
+            view :companies_all,
                  locals: { current_user: @current_account, companies: companies }
           else
             routing.redirect '/auth/login'
