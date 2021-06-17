e00a4ee9f84635d4c977f3750a6b34beefd814f7
havestopchai <havestopchai@yahoo.com.tw>
2021-06-17 12:58:09 +0800

    [view]

---
+++ app/presentation/public/js/datepicker.js
@@ -0,0 +1,11 @@
+$(function () {
+    $("#datepicker").datepicker({
+        // changeMonth: true,
+        // changeYear: true,
+        // showButtonPanel: true,
+        dateFormat: 'yy / mm / dd',
+
+    });
+
+});
+
